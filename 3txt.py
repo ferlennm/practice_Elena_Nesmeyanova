3f9544407a5e9168dc47fb6d23f89511806c3897
ferlennm <121229332+ferlennm@users.noreply.github.com>
2023-07-05 12:45:59 +0300

    Update 3txt.py

--- 3txt.py
+++ 3txt.py
@@ -1,24 +1,24 @@
-n = int(input())
-strt = list(map(int, input().split()))
+n = int(input()) #считываем число n (длину улицы)
+strt = list(map(int, input().split())) #считываем список чисел
 
-distances = [-1] * n
+distances = [-1] * n #cоздаем список distances и заполняем его значениями -1
 length = n
 
-for i in range(n):
-    if strt[i] == 0:
-        length = 0
-    elif length != n:
-        length += 1
-    distances[i] = length
+for i in range(n): #проходим по каждому элементу в списке strt
+    if strt[i] == 0:        #если текущий элемент равен 0
+        length = 0          #устанавливаем length как 0
+    elif length != n:       #если length не равно n
+        length += 1         #увеличиваем length на 1
+    distances[i] = length   #присваиваем distances[i] значение length
 
-length = n
+length = n 
 
-for i in range(n - 1, -1, -1):
-    if strt[i] == 0:
-        length = 0
-    elif length != n:
-        length += 1
-    if distances[i] > length:
-        distances[i] = length
+for i in range(n - 1, -1, -1): #проходим по каждому элементу в списке strt в обратном порядке
+    if strt[i] == 0:             #если текущий элемент равен 0
+        length = 0               #устанавливаем length как 0
+    elif length != n:            #если length не равно n
+        length += 1              #увеличиваем length на 1
+    if distances[i] > length:    #если значение distances[i] больше length
+        distances[i] = length    #присваиваем distances[i] значение length
 
-print(" ".join(map(str, distances)))
+print(" ".join(map(str, distances))) #выводим элементы списка distances через пробел
